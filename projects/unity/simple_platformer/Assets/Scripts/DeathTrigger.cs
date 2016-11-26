ebfd0b39e0723b8fd9d1c8668305f265dd8e3484
jonathantsang <j26tsang@gmail.com>
2016-11-26 13:06:48 -0500

    unity

---
+++ projects/unity/simple_platformer/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player"))
+			Application.LoadLevel (Application.loadedLevel);
+	}
+}
